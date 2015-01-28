7d36fb493431f5a26e52f371c6aa986fbd815f73
mahmoudfelfel <mahmoudfelfel@gmail.com>
2015-01-28 20:34:30 +0200

    Implementing the autocomplete search feature

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -9,35 +9,47 @@
  */
 angular.module('gistApp')
     .controller('mainCtrl', function($scope, searchService) {
-        
+
         $scope.forksList = [];
         $scope.collapsedView = true;
         $scope.fullView = false;
-        
-        $scope.searchGists = function() {
+
+        $scope.searchGists = function(userName) {
             $scope.loading = true;
-            searchService.getGists($scope.searchKeyword).then(function(data) {
+            $scope.showSearchWrapper=false;
+            var searchKeyword = userName || $scope.searchKeyword;
+
+            searchService.getGists(searchKeyword).then(function(data) {
                 $scope.gistList = data;
                 $scope.loading = false;
+                // $scope.showSearchWrapper=false;
             });
         };
 
-        $scope.getForks = function(gistId){
-            searchService.getForks(gistId).then(function(data){
+        $scope.getForks = function(gistId) {
+            searchService.getForks(gistId).then(function(data) {
                 $scope.forksList[gistId] = data;
             });
-        }
+        };
 
-        $scope.expandGist = function(gistId){
+        $scope.expandGist = function(gistId) {
             $scope.fullView = true;
             $scope.collapsedView = false;
 
         };
 
-        $scope.collapseGist = function(){
+        $scope.collapseGist = function() {
             $scope.fullView = false;
             $scope.collapsedView = true;
 
+        };
+
+        $scope.getSearchCompeletions = function() {
+            $scope.loading = true;
+            searchService.getSearchCompeletions($scope.searchKeyword).then(function(data) {
+                $scope.autocompleteList = data;
+                $scope.loading = false;
+            });
         };
 
     });
