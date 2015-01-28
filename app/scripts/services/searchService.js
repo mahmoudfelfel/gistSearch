4e9637d9d57ea37ed09b9e3d0306922974934529
mahmoudfelfel <mahmoudfelfel@gmail.com>
2015-01-28 16:06:43 +0200

    add users search service for autocomplete feature

--- app/scripts/services/searchService.js
+++ app/scripts/services/searchService.js
@@ -66,10 +66,33 @@
             return deferred.promise;
         };
 
+        var getSearchCompeletions = function getForks(userName) {
+            var deferred = $q.defer();
+            // the https://bitpickio.herokuapp.com/ is a simple node proxy to handle the cros-origin issue, i built it before for another project and reusing it here
+            $http({
+                url: "https://bitpickio.herokuapp.com/https://api.github.com/search/users?q=" + userName,
+                method: 'GET',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            }).
+            success(function(data) {
+                deferred.resolve(data);
+            }).
+            error(function(data, status, headers, config) {
+                deferred.reject({
+                    message: "Error!"
+                });
+            });
+            return deferred.promise;
+        };
+
+
         return {
             getGists: getGists,
             getForks: getForks,
-            getGist: getGist
+            getGist: getGist,
+            getSearchCompeletions: getSearchCompeletions
         };
     });
 })(window.angular);
